f3083e19a5d3a189a70ad551e4bd02bb4fff304e
Max Berta <mberta@apple.com>
2018-04-23 13:52:21 +0100

    implemented upload and response

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const express = require('express')
 const app = express()
+var multer  = require('multer')
+var upload = multer({ dest: 'uploads/' })
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.use(express.static('public'))
+
+app.post('/upload', upload.single('userfile'), function (req, res, next) {
+var userfile={}
+userfile.size=req.file.size  
+res.json(userfile)
+})
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
